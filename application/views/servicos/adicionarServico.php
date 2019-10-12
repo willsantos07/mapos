d6985c989a5d68076fa61c841cd5e17e1047ed71
willsantos07 <will0404@hotmail.com>
2019-10-12 08:22:09 -0300

    correção icon

--- application/views/servicos/adicionarServico.php
+++ application/views/servicos/adicionarServico.php
@@ -3,7 +3,7 @@
         <div class="widget-box">
             <div class="widget-title">
                 <span class="icon">
-                    <i class="icon-align-justify"></i>
+                    <i class="icon-wrench"></i>
                 </span>
                 <h5>Cadastro de Serviço</h5>
             </div>
